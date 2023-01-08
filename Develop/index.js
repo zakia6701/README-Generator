c1667149a1eb2b6d4aeb10186cdc55fe1c2d02d5
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2023-01-08 10:32:47 -0600

    files

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [(
+const questions = [{
         type:  "input",
      message: "what is the name of the your  project?", 
      name: "title"
-    )];
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {} (
-    inquire.prompt(questions)
+    inquirer.prompt(questions)
 )
 
 // Function call to initialize app
