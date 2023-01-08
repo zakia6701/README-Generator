c0970bacee3a9f2873f5f57087c271c4bfb93f61
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2023-01-08 10:20:26 -0600

    nc

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [(
+        type:  "input",
+     message: "what is the name of the your  project?", 
+     name: "title"
+    )];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {} (
+    inquire.prompt(questions)
+)
+
+// Function call to initialize app
+init();
